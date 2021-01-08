335a06903768a5cc4bdab22e80fd4b399c8fd381
hoklavat <barisyildiz1982@gmail.com>
2021-01-08 21:43:17 +0300

    .

--- 02-Constant.cpp
+++ 02-Constant.cpp
@@ -7,10 +7,8 @@ int x = 1;
 const int y = x;
 //constexpr int z = x; //error.
 
-
 //double f(double x){return x*x;}
 constexpr double f(double x){return x*x;}
 constexpr int z = f(10);
-
 
 int main(){}
