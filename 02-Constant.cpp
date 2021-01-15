d3d502eb930f362150594440edb8560c950091aa
hoklavat <barisyildiz1982@gmail.com>
2021-01-15 11:31:21 +0300

    .

--- 02-Constant.cpp
+++ 02-Constant.cpp
@@ -11,4 +11,12 @@ const int y = x;
 constexpr double f(double x){return x*x;}
 constexpr int z = f(10);
 
+//In-Class Constant.
+class C{
+public:
+	static const int c = 1; //can be referenced by pointer outside of class. const int* p = &C::c;
+};
+const int C::c; //definition. otherwise not lvalue error.
+
+
 int main(){}
