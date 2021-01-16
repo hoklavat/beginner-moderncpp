89b5c4972d4e31b162e6bedc95e8e31255078c58
hoklavat <barisyildiz1982@gmail.com>
2021-01-16 18:13:26 +0300

    .

--- 31-NamedCast.cpp
+++ 31-NamedCast.cpp
@@ -6,7 +6,7 @@ int main(){
 	//reinterpret_cast. conversion between unrelated types.
 	char c1 = 'a';
 	std::cout << (int)&c1 << "-" << c1 << std::endl;
-	int *i = reinterpret_cast<int*>(&c1); //float and int are same sized. 4-bytes.
+	int *i = reinterpret_cast<int*>(&c1); //char and int are unrelated.
 	std::cout << (int)i << "-" << *i << std::endl;
 	char* c2 = reinterpret_cast<char*>(i);
 	std::cout << (int)c2 << "-" << *c2 << std::endl;
